f1ed2eba53d46cc8e44dc759dd33c1d95a230ab6
russell <rrlittle2@gmail.com>
2018-12-13 16:28:32 -0600

    russ updates

--- powerline_shell/segments/set_term_title.py
+++ powerline_shell/segments/set_term_title.py
@@ -1,22 +1,35 @@
 import os
 import socket
-from ..utils import BasicSegment
+from ..utils import BasicSegment, warn
 
 
 class Segment(BasicSegment):
+    def format(self, shell, content):
+        wrappers = {
+            'bash': '\\[\\e]0;%s\\a\\]',
+            'zsh': '%{\033]0;%s\007%}'
+        }
+        default = '\033]0;%s\007'
+        return wrappers.get(shell, default).format(content)
+
+    def build_title(self, title):
+        if type(title) is str:
+            return title
+        elif type(title) is list:
+            warn('not implemented yet')
+        else:
+            return 'powerline-shell set_term_title title invalid'
+
     def add_to_powerline(self):
         powerline = self.powerline
         term = os.getenv('TERM')
         if not (('xterm' in term) or ('rxvt' in term)):
             return
-        if powerline.args.shell == 'bash':
-            set_title = '\\[\\e]0;\\u@\\h: \\w\\a\\]'
-        elif powerline.args.shell == 'zsh':
-            set_title = '%{\033]0;%n@%m: %~\007%}'
-        else:
-            set_title = '\033]0;%s@%s: %s\007' % (
-                os.getenv('USER'),
-                socket.gethostname().split('.')[0],
-                powerline.cwd,
-            )
-        powerline.append(set_title, None, None, '')
+        cust_title = powerline.segment_conf('set_term_title', 'title')
+        content = '%s@%s: %s' % (
+            os.getenv('USER'),
+            socket.gethostname().split('.')[0],
+            powerline.cwd) if cust_title else self.build_title(cust_title)
+
+        set_title = self.format(powerline.args.shell, content)
+        powerline.append(set_title, None, None, None)
